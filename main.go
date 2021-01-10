2b37ea04963f5d874cfda84fab2398b4d6a38351
bee-plus <bplusx@protonmail.com>
2021-01-10 21:45:04 +0000

    Comments and stuff

--- main.go
+++ main.go
@@ -16,54 +16,64 @@ var (
 	Token string
 	// Commands is a map of commands and their outputs.
 	Commands map[string]string
-	// ApprovedOnly defines if only approved users may use bot commands.
-	ApprovedOnly bool
-	// IDs is a slice of user IDs approved to use bot commands.
-	IDs []string
+	// WhitelistEnabled defines if only approved users may use bot commands.
+	WhitelistEnabled bool
+	// Whitelist is a slice of user IDs approved to use bot commands.
+	Whitelist []string
 	// ConfigLoaded defines if the config has been loaded.
 	ConfigLoaded bool
 )
 
 // Config defines the YAML config data structure.
 type Config struct {
-	Commands     map[string]string `yaml:"commands"`
-	ApprovedOnly bool              `yaml:"approved_only"`
-	IDs          []string          `yaml:"ids"`
+	Commands         map[string]string `yaml:"commands"`
+	WhitelistEnabled bool              `yaml:"whitelist_enabled"`
+	Whitelist        []string          `yaml:"whitelist"`
 }
 
 func loadConfig() {
 	var config Config
 
+	// Open config file.
 	file, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
 		if !ConfigLoaded {
+			// If no config has been loaded previously, exit.
 			log.Fatal(err)
 		} else {
+			// If a config has been loaded previously, do nothing.
 			log.Println(err)
 			return
 		}
 	}
 
+	// Unmarshal config file.
 	err = yaml.UnmarshalStrict(file, &config)
 	if err != nil {
 		if !ConfigLoaded {
+			// If no config has been loaded previously, exit.
 			log.Fatal(err)
 		} else {
+			// If a config has been loaded previously, do nothing.
 			log.Println(err)
 			return
 		}
 	}
 
+	// Export values to global variables.
 	Commands = config.Commands
-	IDs = config.IDs
-	ApprovedOnly = config.ApprovedOnly
+	WhitelistEnabled= config.WhitelistEnabled
+	Whitelist = config.Whitelist
 
+	// Success!
 	ConfigLoaded = true
 	log.Println("config loaded successfully")
 }
 
 func init() {
+	// Get API token from environment.
 	Token = os.Getenv("TOKEN")
+	// Load config file.
 	loadConfig()
 }
 
@@ -90,18 +100,19 @@ func main() {
 
 	// Wait here until CTRL-C or other term signal is received.
 	log.Println("running; press ctrl-c to exit")
+
 	rc := make(chan os.Signal, 1)
-	// Reload config on SIGHUP.
 	signal.Notify(rc, syscall.SIGHUP)
+	// Reload config on SIGHUP.
 	go func() {
 		for range rc {
 			loadConfig()
 		}
 	}()
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
-
 	// Cleanly close down the Discord session.
 	log.Println("exiting...")
 	err = dg.Close()
@@ -118,8 +129,9 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if ApprovedOnly {
-		for _, id := range IDs {
+	// Determine if the author is approved to use the bot.
+	if WhitelistEnabled {
+		for _, id := range Whitelist {
 			if id == m.Author.ID {
 				approved = true
 				break
@@ -129,9 +141,15 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		approved = true
 	}
 
-	val, isCmd := Commands[m.Content]
+	// If the author is not approved, do nothing.
+	if !approved {
+		return
+	}
 
-	if isCmd && approved {
+	// Check if the message is a command.
+	val, isCmd := Commands[m.Content]
+	if isCmd {
+		// Send a message corresponding to the given command.
 		_, err := s.ChannelMessageSend(m.ChannelID, val)
 		if err != nil {
 			log.Println(err)
